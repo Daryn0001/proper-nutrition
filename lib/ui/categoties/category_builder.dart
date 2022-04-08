f28ee5b5c086bd15a0a1a3bf2060e54f15d49401
Daryn0001 <53093184+Daryn0001@users.noreply.github.com>
2022-04-08 15:35:47 +0600

    init

--- lib/ui/categoties/category_builder.dart
+++ lib/ui/categoties/category_builder.dart
@@ -20,25 +20,58 @@ class CategoryBuilder extends StatefulWidget {
 class _CategoryBuilderState extends State<CategoryBuilder> {
 
 
-  Text getText(){
-    return Text("");
+
+  Container getBackground() {
+    return Container(
+
+      child: getText(),
+    );
+  }
+
+  Container getText(){
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.65,
+      height: MediaQuery.of(context).size.width * 0.15,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: const Color(0xff358c6a),
+        border: Border.all(
+            color: const Color(0xff000000),
+            width: 1,
+            style: BorderStyle.solid,
+        )
+      ),
+      child: Center(
+        child: Text(
+          widget.text,
+          style: const TextStyle(
+            fontSize: 24,
+            color:  Color(0xffffffff),
+            fontWeight: FontWeight.w600,
+          ),
+
+        ),
+      ),
+    );
   }
 
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height * 0.3;
-    double width = MediaQuery.of(context).size.width * 0.9;
     return Container(
       height: height,
-      width: width,
-      color: widget.color,
+      decoration: const BoxDecoration(
+        color: Color(0xff555555),
+        image: DecorationImage(
+          image:  AssetImage('assets/images/1306009.jpg'),
+          fit: BoxFit.cover,
+        ),),
+
       margin: const EdgeInsets.symmetric(
         vertical: 2.0 ,
       ),
-      child: Center(
-          child: Text(widget.text)
-      ),
+      child: Center(child: getText()),
     );
   }
 }
--- lib/ui/screens/category_screen.dart
+++ lib/ui/screens/category_screen.dart
@@ -1,36 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:proper_nutrition_app/ui/categoties/category_builder.dart';
 
-class CategoryScreen extends StatefulWidget{
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
   @override
   _CategoryScreenState createState() => _CategoryScreenState();
-
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-
-   List<CategoryBuilder> list = [const CategoryBuilder(color: Color(0xffda2c02), text: 'завтрак',),
-     const CategoryBuilder(color: Color(0xff94dd16), text: 'Обед',),
-     const CategoryBuilder(color: Color(0xff6880e6), text: 'Ужин',),
-     const CategoryBuilder(color: Color(0xffdf1ca4), text: 'Дополнение',),
-   ];
+  List<CategoryBuilder> list = [
+    const CategoryBuilder(
+      color: Color(0xffda2c02),
+      text: 'завтрак',
+    ),
+    const CategoryBuilder(
+      color: Color(0xff94dd16),
+      text: 'Обед',
+    ),
+    const CategoryBuilder(
+      color: Color(0xff6880e6),
+      text: 'Ужин',
+    ),
+    const CategoryBuilder(
+      color: Color(0xffdf1ca4),
+      text: 'Дополнение',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return  Expanded (
-      child: ListView(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          children: const [
-             CategoryBuilder(color: Color(0xffda2c02), text: 'завтрак',),
-             CategoryBuilder(color: Color(0xff94dd16), text: 'Обед',),
-             CategoryBuilder(color: Color(0xff6880e6), text: 'Ужин',),
-             CategoryBuilder(color: Color(0xffdf1ca4), text: 'Дополнение',)
-          ]
+    return Container(
+      color: const Color(0xff000000),
+      child: Flex(
+        direction: Axis.vertical,
+        children: [
+          Expanded(
+            flex: 2,
+            child: ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: const [
+                  CategoryBuilder(
+                    color: Color(0xffda2c02),
+                    text: 'завтрак',
+                  ),
+                  CategoryBuilder(
+                    color: Color(0xff94dd16),
+                    text: 'Обед',
+                  ),
+                  CategoryBuilder(
+                    color: Color(0xff6880e6),
+                    text: 'Ужин',
+                  ),
+                  CategoryBuilder(
+                    color: Color(0xffdf1ca4),
+                    text: 'Дополнение',
+                  )
+                ]),
+          ),
+        ],
       ),
     );
   }
-
 }
