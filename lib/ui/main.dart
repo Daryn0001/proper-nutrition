432055d6dc9efefd4bc8ccdeba0b107c4ad8e3ea
Daryn0001 <53093184+Daryn0001@users.noreply.github.com>
2022-04-08 15:54:12 +0600

    init add assets/images

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -15,6 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        tabBarTheme: const TabBarTheme(),
+      ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -22,19 +25,42 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+
+
+
+
+
   final List<Tab> tabs = <Tab>[
-    const Tab(child: RecipesScreen(), icon: Icon(Icons.dining_outlined )),
+    const Tab(child: RecipesScreen(), icon: Icon(Icons.dining_outlined)),
     const Tab(
         child: CategoryScreen(),
         icon: Icon(Icons.format_list_bulleted_outlined)),
     const Tab(child: DietsScreen(), icon: Icon(Icons.filter_3)),
-    const Tab(child: FactsScreen(), icon: Icon(Icons.auto_stories_outlined )),
+    const Tab(child: FactsScreen(), icon: Icon(Icons.auto_stories_outlined)),
   ];
+
 
   HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final TabBar _tabBar = TabBar(
+      indicatorColor: const Color(0xff016725),
+
+      tabs: tabs.map((Tab tab) {
+        return Column(
+          children: [
+            Container(
+              child: tab.icon,
+            ),
+            Text(tab.text ?? 'Unknown'),
+          ],
+        );
+      }).toList(),
+    );
+
+
     return DefaultTabController(
       length: 4,
       child: Scaffold(
@@ -45,22 +71,16 @@ class HomePage extends StatelessWidget {
                   fontSize: 20,
                   fontWeight: FontWeight.w600,
                   color: Colors.white)),
-          bottom: TabBar(
-            tabs: tabs.map((Tab tab) {
-              return Column(
-                children: [
-                  Container(
-                    child: tab.icon,
-                  ),
-                  Text(tab.text ?? 'Unknown'),
-                ],
-              );
-            }).toList(),
+          bottom: PreferredSize(
+            preferredSize: _tabBar.preferredSize,
+            child: ColoredBox(
+              color: const Color(0xff016725),
+              child: _tabBar,
+            ),
           ),
         ),
         body: TabBarView(
             children: tabs.map((Tab tab) {
-          //final String label = tab.text!.toLowerCase();
           return Container(
             child: tab.child,
           );
