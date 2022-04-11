f34b72e054aa61ccd5f2c28a1cab0e7fe3ecda94
Daryn0001 <53093184+Daryn0001@users.noreply.github.com>
2022-04-11 20:11:08 +0600

    init

---
+++ lib/ui/dishes/dish.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dish.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Dish _$DishFromJson(Map<String, dynamic> json) => Dish(
+      json['favorite'] as bool,
+      json['title'] as String,
+      json['category_id'] as int,
+      json['diet_id'] as int,
+      json['image_id'] as int,
+      json['excerpt'] as String,
+      json['power'] as int,
+      (json['ingredients'] as List<dynamic>)
+          .map((e) => Ingredient.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      json['advice'] as String,
+      (json['cooking_method'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+    );
+
+Map<String, dynamic> _$DishToJson(Dish instance) => <String, dynamic>{
+      'favorite': instance.favorite,
+      'title': instance.title,
+      'category_id': instance.categoryID,
+      'diet_id': instance.dietID,
+      'image_id': instance.imageID,
+      'excerpt': instance.description,
+      'power': instance.numberOfCalories,
+      'ingredients': instance.ingredients,
+      'advice': instance.advice,
+      'cooking_method': instance.cookingMethod,
+    };
---
+++ lib/ui/dishes/ingredient.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredient.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Ingredient _$IngredientFromJson(Map<String, dynamic> json) => Ingredient(
+      json['name'] as String,
+      json['type'] as int,
+      json['quantity'] as int,
+    );
+
+Map<String, dynamic> _$IngredientToJson(Ingredient instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'type': instance.type,
+      'quantity': instance.quantity,
+    };
